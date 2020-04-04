b746a6636ba4c8e640ca19926f870f245d910876
Darryl Mangibin <darrylmangibin@gmail.com>
2020-04-04 12:16:24 +0800

    private links

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 import { AuthButton } from "../common";
 
-const HomeView = () => {
+const HomeView = ({ authenticated, history }) => {
+	useEffect(() => {
+		console.log(authenticated)
+		if (authenticated) {
+			history.push("/notes");
+		}
+		// eslint-disable-next-line
+	}, [authenticated]);
 	return (
-		<div id="home" className="container" style={{ height: `calc(100vh - 90px)` }}>
-			<div style={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+		<div
+			id="home"
+			className="container"
+			style={{ height: `calc(100vh - 90px)` }}
+		>
+			<div
+				style={{
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-around",
+				}}
+			>
 				<AuthButton to="/login">Sign in</AuthButton>
 				<AuthButton to="/register" secondary>
 					Sign up
@@ -15,4 +35,8 @@ const HomeView = () => {
 	);
 };
 
-export default HomeView;
+const mapStateToProps = (state) => ({
+	authenticated: state.auth.authenticated,
+});
+
+export default connect(mapStateToProps)(withRouter(HomeView));
--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({ authenticated }) => {
 	return (
 		<div className="actions">
 			<div className="actions__container actions__container--spaced">
-				<Link to="/">Home</Link>
-				<Link to="/notes">Notes</Link>
-				<Link to="/notes/create">Create</Link>
+				<Link to={authenticated ? "/notes" : "/"}>Home</Link>
+				{authenticated && (
+					<Fragment>
+						<Link to="/notes">Notes</Link>
+						<Link to="/notes/create">Create</Link>
+					</Fragment>
+				)}
 				<Link to="/about">About</Link>
 			</div>
 		</div>
 	);
 };
 
-export default Nav;
+const mapStateToProps = (state) => ({
+	authenticated: state.auth.authenticated,
+});
+
+export default connect(mapStateToProps)(Nav);
